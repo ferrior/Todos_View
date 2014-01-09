6e9c6aa6c3651337073cd82408238138989159d6
Bin Liu <bliu.howto@gmail.com>
2014-01-09 14:39:58 +0800

    Add a module, so that more than one controller can be defined.

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,5 @@
-function todosController ($scope){
+var todosApp =  angular.module('todosApp', []);
+
+todosApp.controller('todosController',function ($scope){
 	$scope.todos = [{title:'title1'}, {title:'title2'}];
-}
+});
