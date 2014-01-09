9f3f1f3b2cd7ab8440da2f4f8322b799a3bc1227
Bin Liu <bliu.howto@gmail.com>
2014-01-09 14:47:33 +0800

    Apply template

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,12 @@
 var todosApp =  angular.module('todosApp', []);
 
+todosApp.config(function ($routeProvider){
+	$routeProvider.when('/todos', {template:'test1'}).
+	when('/todos/:id', {template:'test2'}).
+	otherwise({redirectTo:'/todos'});
+}
+);
+
 todosApp.controller('todosController',function ($scope){
 	$scope.todos = [{title:'title1'}, {title:'title2'}];
 });
