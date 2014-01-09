948ed0d9de9619458e6273b57a0d8071375182bc
Bin Liu <bliu.howto@gmail.com>
2014-01-09 12:10:54 +0800

    Use local storage

--- public/js/client.js
+++ public/js/client.js
@@ -4,41 +4,43 @@ require.config({
 		'$':'jquery',
 		'tmpl':'handlebars',
 		'em':'ember',
-		'ember_data':'ember-data'
+		'ember_data':'ember-data',
+		'ls':'localstorage_adapter'
 	}
 });
 
 require(['$','tmpl','em'], function(){
 	require(['ember-data'], function(){//ember-data require em, so load em firstly.
-		window.App = Em.Application.create();
-		/*App.ApplicationView = Em.View.extend({
-			templateName:"todos"
-		});*/
-		App.Router.map(function(){
-			this.route('todos',{path:'/'});
+		require(['ls'], function(){
+			window.App = Em.Application.create();
+			App.Router.map(function(){
+				this.route('todos',{path:'/'});
+			});
+			App.Todo = DS.Model.extend({
+				title:DS.attr('string'),
+				isCompleted:DS.attr('boolean')
+			});
+			App.ApplicationAdapter = DS.LSAdapter.extend({
+				namespace:'todos'
+			});
+			App.TodosRoute = Em.Route.extend({
+				model:function(){
+					return this.store.find('todo');
+				}
+			});
+			App.TodosController = Em.ArrayController.extend({
+				actions:{
+					createTodo:function(){
+						var newTitle = this.newTitle.trim();
+						if(!newTitle) return;
+						var todo = this.get('store').createRecord('todo',{
+							title:newTitle
+						});
+						todo.save();
+						this.set('newTitle','');
+					}
+				}
+			});
 		});
-		App.Todo = DS.Model.extend({
-			title:DS.attr('string'),
-			isCompleted:DS.attr('boolean')
-		});
-		//begin FIXTURES
-		App.ApplicationAdapter = DS.FixtureAdapter.extend();
-		App.Store = DS.Store.extend({
-			adapter:'DS.FixtureAdapter'
-		});
-		//假数据
-		App.Todo.FIXTURES = [
-			{id:1, title:'todo1', isCompleted:true},
-			{id:2, title:'todo2', isCompleted:true},
-			{id:3, title:'todo3', isCompleted:true}
-		];
-		//end 
-		App.TodosRoute = Em.Route.extend({
-			model:function(){
-				return this.store.find('todo');
-
-			}
-		});
-		App.TodosController = Em.ArrayController.extend();
 	});
 });
