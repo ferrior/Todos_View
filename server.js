54a4632bd610c01253fc03843304fb3c1b052721
Bin Liu <bliu.howto@gmail.com>
2014-01-09 14:10:09 +0800

    access mongodb. do not tested.

--- server.js
+++ server.js
@@ -2,18 +2,31 @@ var express = require('express'),
 mongoose = require('mongoose'),
 app = express();
 
+mongoose.connect("mongodb://localhost:27017/todos");
+var Todo = mongoose.model('Todo', new mongoose.Schema({
+	id:{
+		type:Number,
+		index:true
+	},
+	title:String,
+	isCompleted:Boolean
+}));
+
 app.use(express.bodyParser());
 app.use(express.static('public'));
 
 app.get('/todos', function(req,res){
-	var todos = [{id:1,title:'todoa'}, {id:2,title:'todo2'}];
-	res.send({
-		todos:todos
+	Todo.find({}, function(err,data){
+		res.send({
+			todos:data
+		});
 	});
+
 });
 
 app.post('/todos', function(req,res){
-	console.log(req.body.todo);
+	var todo = new Todo(req.body.todo);
+	todo.save();
 });
 
 app.listen(9432,function(){
